4357fe9524f8c2f03bda7b933c4c07ffcabb8b98
jiwan <jiwan@rezi.io>
2025-11-21 14:53:33 +0900

    livekit-protocol

---
+++ observability/reporter.go
@@ -0,0 +1,48 @@
+package observability
+
+import (
+	"github.com/livekit/protocol/observability/agentsobs"
+	"github.com/livekit/protocol/observability/gatewayobs"
+	"github.com/livekit/protocol/observability/roomobs"
+	"github.com/livekit/protocol/observability/telephonyobs"
+)
+
+const Project = "livekit"
+
+type Reporter interface {
+	Room() roomobs.Reporter
+	Agent() agentsobs.Reporter
+	Gateway() gatewayobs.Reporter
+	Telephony() telephonyobs.Reporter
+	Connector() any // any is a placeholder for the connector type
+	Close()
+}
+
+func NewReporter() Reporter {
+	return reporter{}
+}
+
+type reporter struct{}
+
+func (reporter) Room() roomobs.Reporter {
+	return roomobs.NewNoopReporter()
+}
+
+func (reporter) Agent() agentsobs.Reporter {
+	return agentsobs.NewNoopReporter()
+}
+
+func (reporter) Gateway() gatewayobs.Reporter {
+	return gatewayobs.NewNoopReporter()
+}
+
+func (reporter) Telephony() telephonyobs.Reporter {
+	return telephonyobs.NewNoopReporter()
+}
+
+func (reporter) Connector() any {
+	return nil
+}
+
+func (reporter) Close() {
+}
